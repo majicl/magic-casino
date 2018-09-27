dd3f68d12ba87a3df13a6901c4b2cffb083fc185
Majid Parvin <parvin.majid@hedcoint.com>
2018-09-27 16:32:39 +0330

    init gulp to manage asset files

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+
+const minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const envs = {
+    production: 'production',
+    development: 'development'
+};
+
+const getCssPipes = (env) => {
+    let src = gulp.src('assets/styles/**/*');
+    let destPath = 'public/assets/styles';
+    if (env === envs.production) {
+        return src
+            .pipe(minifyCSS())
+            .pipe(gulp.dest(destPath));
+    } else {
+        return src
+            .pipe(gulp.dest(destPath));
+    }
+}
+
+const getJsPipes = (env) => {
+    let src = gulp.src('assets/scripts/**/*.js');
+    let destPath = 'public/assets/scripts';
+    if (env === envs.production) {
+        return src.pipe(sourcemaps.init())
+            .pipe(concat('app.min.js'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(destPath));
+    } else {
+        return src.pipe(gulp.dest(destPath));
+    }
+}
+
+gulp.task('clean', () => {
+    return gulp.src(
+        'public/assets',
+        { read: false }
+    ).pipe(clean());
+});
+
+gulp.task('css', () => {
+    return getCssPipes();
+});
+
+gulp.task('js', () => {
+    return getJsPipes();
+});
+
+gulp.task('default', ['clean'], () => {
+    gulp.start(['css', 'js']);
+});
